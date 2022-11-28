a10a0c3131625626e56ef3dfa8b56c30dd970e35
Sofia <sbarcelo@alumnos.exa.unicen.edu.ar>
2022-11-28 15:11:01 -0300

    Agrega licencia prestashop

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInit4a464c3cc6d4130513250579dbd196d2
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit4a464c3cc6d4130513250579dbd196d2::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire4a464c3cc6d4130513250579dbd196d2($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire4a464c3cc6d4130513250579dbd196d2($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }

5c3dc988c59fb32ed39585438e893754a386968d
Sofia <sbarcelo@alumnos.exa.unicen.edu.ar>
2022-11-28 14:37:01 -0300

    Soluciona problema standards

--- src/Classes/OcaEpakOrder.php
+++ src/Classes/OcaEpakOrder.php
@@ -234,8 +234,13 @@ class OcaEpakOrder extends ObjectModel
                         break;
                 }
             }
-            if (isset($ocaAddress['geocoded country'])) {   
-                $ocaAddress = ['floor' => $ocaAddress['floor'],'apartment' => $ocaAddress['apartment'],'other' => $ocaAddress['other'],'geocoded' => true];
+            if (isset($ocaAddress['geocoded country'])){   
+                $ocaAddress = [
+                'floor' => $ocaAddress['floor'],
+                'apartment' => $ocaAddress['apartment'],
+                'other' => $ocaAddress['other'],
+                'geocoded' => true,
+                ];
             }
         } else {
             $ocaAddress['geocoded'] = false;
